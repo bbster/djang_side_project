a3fe41882d2a873f6bd3d426f60c734a3ba1463f
bbster <bbster12@naver.com>
2021-07-06 20:43:28 +0900

    git global config수정

--- boards/views.py
+++ boards/views.py
@@ -22,21 +22,3 @@ class BoardViewset(viewsets.ModelViewSet):
         """
         breakpoint()
         return serializer.save(user=self.request.user)
-
-    # def list(self, request):
-    #     pass
-    #
-    # def create(self, request):
-    #     pass
-    #
-    # def retrieve(self, request, pk=None):
-    #     pass
-    #
-    # def update(self, request, pk=None):
-    #     pass
-    #
-    # def partial_update(self, request, pk=None):
-    #     pass
-    #
-    # def destroy(self, request, pk=None):
-    #     pass
